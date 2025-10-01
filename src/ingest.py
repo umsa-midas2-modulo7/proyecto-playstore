865302de30d1133e8ea67c29c851541550a4c63c
Marco VOM <mvladyom@gmail.com>
2025-10-01 00:25:52 -0400

    ingesta/update: funcion get_reviews

--- src/ingest.py
+++ src/ingest.py
@@ -13,13 +13,32 @@ from common.constants import AppConstants
 
 app_ids = AppConstants.APP_IDS
 
+# Función para obtener reseñas desde una fecha específica o la fecha de hoy por defecto, sin captura de errores
 
-def reviews_por_fecha(app_id, fecha:date = None):
+
+def get_reviews(result, app_id, fecha):
+    resenas = []
+    hoy = date.today()  # para la fecha de hoy
+    token = True  # Inicializar como True para indicar que seguimos
+
+    for r in result:
+        review_date = pd.Timestamp(r['at']).normalize()
+        comparison_date = fecha if fecha else hoy
+
+        if review_date.date() >= comparison_date:
+            r['app_id'] = app_id
+            resenas.append(r)
+        else:
+            token = None  # Detener la paginación si la reseña es más antigua
+            break
+
+    return resenas, token
+
+
+def reviews_por_fecha(app_id, fecha: date = None):
 
     all_app_reviews = []
     token = None
-    # para la fecha de hoy
-    hoy = date.today()
 
     while True:
         result, token = reviews(
@@ -34,19 +53,8 @@ def reviews_por_fecha(app_id, fecha:date = None):
         if not result:
             break  # No hay más reseñas
 
-        for r in result:
-            # Normalizar la fecha de la reseña para comparar solo la parte de la fecha
-            review_date = pd.Timestamp(r['at']).normalize()
-            # Usar la fecha de hoy si no se proporciona una fecha, de lo contrario usar la fecha proporcionada
-            comparison_date = fecha if fecha else hoy
-
-            if review_date.date() >= comparison_date:
-                r['app_id'] = app_id
-                all_app_reviews.append(r)
-            else:
-                # Dado que las reseñas están ordenadas por las más recientes, podemos detenernos cuando lleguemos a reseñas más antiguas que la fecha especificada
-                token = None  # Detener la paginación
-                break
+        resenas, token = get_reviews(result, app_id, fecha)
+        all_app_reviews.extend(resenas)
 
         if not token:
             break
@@ -55,11 +63,12 @@ def reviews_por_fecha(app_id, fecha:date = None):
 
     return all_app_reviews
 
-def get_app_reviews(fecha:date = None):
+
+def get_app_reviews(fecha: date = None):
     all_reviews = []
     for app in app_ids:
-        print(f"recolectando {app} ...")    
-    # para las reseñas de hoy solo 'reviews_por_fecha(app)' sin fecha 
+        print(f"recolectando {app} ...")
+    # para las reseñas de hoy solo 'reviews_por_fecha(app)' sin fecha
         app_reviews = reviews_por_fecha(app, fecha)
         all_reviews.extend(app_reviews)
         print(f"...finalizado de {app}")
@@ -70,6 +79,7 @@ def get_app_reviews(fecha:date = None):
     '''
     print(f"Total de reseñas descargadas: {len(df)}\nen fecha: {fecha}")
     print(df)
+
 
 print(app_ids)
 la_fecha = date(2025, 9, 29)
