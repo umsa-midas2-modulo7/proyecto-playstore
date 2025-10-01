097c34b89f85bcf80b796a01c20c4c40ae923ccf
Marco VOM <mvladyom@gmail.com>
2025-09-30 23:21:42 -0400

    ingesta/update: actualizar funciones

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,24 +1,25 @@
 from typing import Final, List
-from types.app import AppInfo, App
+from interface.app import AppInfo, App
+
 
 class AppConstants:
-    
+
     APP_OBJ: Final[List[AppInfo]] = [
         {"app_id": "com.bancosol.altoke", "name": "Altoke"},
         {"app_id": "com.bcp.bo.wallet", "name": "Yape"},
-        {"app_id": "bo.com.yolopago", "name": "YoloPago"}, 
+        {"app_id": "bo.com.yolopago", "name": "YoloPago"},
         {"app_id": "com.busa.wallet", "name": "Yasta"},
-        {"app_id": "com.walletapp.mobile", "name": "WalletApp"}
+        {"app_id": "com.walletapp.mobile", "name": "Bille"}
     ]
 
     APP_IDS: Final[List[str]] = [
         app["app_id"] for app in APP_OBJ
     ]
-    
+
     APPS: Final[List[App]] = [
         App(app_id="com.bancosol.altoke", name="Altoke"),
         App(app_id="com.bcp.bo.wallet", name="Yape"),
-        App(app_id="bo.com.yolopago", name="YoloPago"), 
+        App(app_id="bo.com.yolopago", name="YoloPago"),
         App(app_id="com.busa.wallet", name="Yasta"),
-        App(app_id="com.walletapp.mobile", name="WalletApp")
+        App(app_id="com.walletapp.mobile", name="Bille")
     ]
--- src/ingest.py
+++ src/ingest.py
@@ -7,17 +7,19 @@ import random
 from typing import List, Dict, Any, Tuple
 from google_play_scraper import Sort, reviews
 import pandas as pd
+from datetime import date, datetime
 
 from common.constants import AppConstants
 
+app_ids = AppConstants.APP_IDS
 
 
-def reviews_por_fecha(app_id, date=None):
-    
+def reviews_por_fecha(app_id, fecha:date = None):
+
     all_app_reviews = []
     token = None
-    # para la fecha de hoy 
-    hoy = pd.to_datetime('today').normalize()
+    # para la fecha de hoy
+    hoy = date.today()
 
     while True:
         result, token = reviews(
@@ -36,9 +38,9 @@ def reviews_por_fecha(app_id, date=None):
             # Normalizar la fecha de la reseña para comparar solo la parte de la fecha
             review_date = pd.Timestamp(r['at']).normalize()
             # Usar la fecha de hoy si no se proporciona una fecha, de lo contrario usar la fecha proporcionada
-            comparison_date = date.normalize() if date else hoy
+            comparison_date = fecha if fecha else hoy
 
-            if review_date >= comparison_date:
+            if review_date.date() >= comparison_date:
                 r['app_id'] = app_id
                 all_app_reviews.append(r)
             else:
@@ -53,6 +55,26 @@ def reviews_por_fecha(app_id, date=None):
 
     return all_app_reviews
 
+def get_app_reviews(fecha:date = None):
+    all_reviews = []
+    for app in app_ids:
+        print(f"recolectando {app} ...")    
+    # para las reseñas de hoy solo 'reviews_por_fecha(app)' sin fecha 
+        app_reviews = reviews_por_fecha(app, fecha)
+        all_reviews.extend(app_reviews)
+        print(f"...finalizado de {app}")
+    df = pd.DataFrame(all_reviews)
+    '''
+    df.to_csv(f"../data/restore/{df2['at'].dt.date.max()}_comentarios_app.csv",   index=False, encoding="utf-8-sig")
+    print("Total de reseñas guardadas:", len(df))
+    '''
+    print(f"Total de reseñas descargadas: {len(df)}\nen fecha: {fecha}")
+    print(df)
+
+print(app_ids)
+la_fecha = date(2025, 9, 29)
+get_app_reviews(la_fecha)
+
 
 if __name__ == "__main__":
-    df = main()
+    print()
